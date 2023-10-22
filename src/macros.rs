160b74cc88e3b0433687b65515be8bd795618f99
Carlo <cmascherini@gmail.com>
2023-10-22 19:41:02 +0200

    doc

--- src/macros.rs
+++ src/macros.rs
@@ -19,25 +19,89 @@ macro_rules! closure {
     };
 }
 
+/// # Examples
+///```
+/// # use util::map;
+/// let map = map! {
+/// 	1 => "un",
+/// 	2 => "deux",
+/// 	3 => "trois"
+/// };
+///```
 #[macro_export]
 macro_rules! map {
 	($($key:expr => $value:expr),*) => {{
 		let mut map = std::collections::HashMap::new();
-		$(
-			map.insert($key, $value);
-		)*
+		$(map.insert($key, $value);)*
 		map
 	}}
 }
 
+/// # Examples
+/// ```
+/// # use util::set;
+/// let set = set! {
+/// 	"un",
+/// 	"deux",
+/// 	"trois"
+/// };
+/// ```
+#[macro_export]
+macro_rules! set {
+	($($element:expr),*) => {{
+		let mut set = std::collections::HashSet::new();
+		$(set.insert($element);)*
+		set
+	}}
+}
+
+/// ```compile_fail,E0384
+/// # use util::freeze;
+/// let mut a = 3;
+/// a = 4;
+/// freeze!(a);
+/// a = 5;
+/// ```
 #[macro_export]
 macro_rules! freeze {
 	($variable: ident) => { let $variable = $variable; }
 }
 
+/// # Examples
+///```
+/// # use util::make_mutable;
+/// let vec = vec![1, 2, 3];
+/// make_mutable!(vec);
+/// vec.push(4);
+/// ```
 #[macro_export]
 macro_rules! make_mutable {
 	($variable: ident) => { let mut $variable = $variable; }
+}
+
+#[macro_export]
+macro_rules! flat_mod {
+    ($mod_name: ident) => {
+		mod $mod_name;
+		pub use $mod_name::*;
+	};
+}
+
+/// Inspiré du pipeline operator `|>` que l'on trouve dans certains [langages fonctionnel](https://cs3110.github.io/textbook/chapters/hop/pipelining.html)
+/// # Exemples
+/// ```
+/// # use util::pipeline;
+/// fn increment(n: i32) -> i32 { n + 1 }
+/// fn double(n: i32) -> i32 { n * 2 }
+/// let i = pipeline!(5 |> increment |> double |> increment);
+/// let boxed_twelve = pipeline!(2 + 3 => increment => double => Box::new);
+/// ```
+#[macro_export]
+macro_rules! pipeline  {
+    ($value:tt |> $function:path ) => { $function($value) };
+	($value:tt |> $first_operation:path $(|> $function:path)+) => { pipeline!(($first_operation($value)) $(|> $function)+) };
+    ($value:expr => $function:path ) => { $function($value) };
+	($value:expr => $first_operation:path $(=> $function:path)+) => { pipeline!($first_operation($value) $(=> $function)+) };
 }
 
 #[cfg(test)]
@@ -98,13 +162,16 @@ mod tests {
 	}
 
 	#[test]
-	fn freeze() {
-		let mut _vec = vec![1, 2, 3];
-		_vec.push(4);
-
-		freeze!(_vec);
-
-		// _vec.push(5);
+	fn set() {
+		let set = set! {
+			"un",
+			"deux",
+			"trois",
+			"deux"
+		};
+		assert_eq!(set.len(), 3);
+		assert!(set.contains("un"));
+		assert!(set.contains("deux"));
 	}
 
 	#[test]
@@ -112,6 +179,29 @@ mod tests {
 		let vec = vec![1, 2, 3];
 		make_mutable!(vec);
 		vec.push(4);
+	}
+
+	#[test]
+	fn pipeline() {
+		fn increment(n: i32) -> i32 { n + 1 }
+		fn double(n: i32) -> i32 { n * 2 }
+
+		let result = pipeline!(5 |> increment |> double |> increment);
+		assert_eq!(result, 13);
+		let result = pipeline!(5 => increment => double => increment);
+		assert_eq!(result, 13);
+		let result = pipeline!(5
+			|> increment
+			|> double
+			|> increment
+		);
+		assert_eq!(result, 13);
+		let result = pipeline!(5
+			=> increment
+			=> double
+			=> Box::new
+		);
+		assert_eq!(result, Box::new(12))
 	}
 
 }
